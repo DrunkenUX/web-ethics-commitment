c629e6b20b67bf556c43095c1d7e7d8a4c1d3a57
Michael Fienen <fienen@gmail.com>
2021-09-20 23:18:34 -0500

    Initial scaffolding for everything

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+    let markdownIt = require("markdown-it");
+    let options = {
+        html: true,
+        linkify: true
+    };
+
+    eleventyConfig.addPassthroughCopy('src/css');
+    eleventyConfig.setLibrary("md", markdownIt(options));
+
+    return {
+        dir: {
+            input: "src",
+            output: "dist"
+        },
+        passthroughFileCopy: true
+    }
+};
