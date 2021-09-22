7f3af2f2d9d01d64e172c191fb7fe90499f5eebd
Michael Fienen <fienen@gmail.com>
2021-09-22 18:02:06 -0500

    Fixes #2

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,31 @@
-const env = process.env.NODE_ENV === `prod`;
+const env        = process.env.NODE_ENV === `prod`;
+const htmlmin    = require("html-minifier");
+const markdownIt = require("markdown-it");
+
+let options = {
+    html: true,
+    linkify: true
+};
 
 module.exports = function(eleventyConfig) {
-    let markdownIt = require("markdown-it");
-    let options = {
-        html: true,
-        linkify: true
-    };
-
     eleventyConfig.addPassthroughCopy('src/css');
     eleventyConfig.addPassthroughCopy('src/img');
+    
     eleventyConfig.setLibrary("md", markdownIt(options));
+
+    eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+        if( outputPath && outputPath.endsWith(".html") ) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+
+            return minified;
+        }
+    
+        return content;
+    });
 
     return {
         dir: {
